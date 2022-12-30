cb399d1e322c8774d6554599903fec519a6f6867
nguyen kien <nkien6962@gmail.com>
2022-12-31 04:51:11 +0700

    no message

--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -31,23 +31,25 @@ export class OrderService {
     console.log('totalPrice', totalPrice);
 
     const products = await this.productService.findProductsByIds(productsIds);
+    if (products.length > 0) {
+      const createOrder = new OrderEntity(createOrderDto);
+      createOrder.userId = userId;
+      createOrder.totalPrice = totalPrice;
+      console.log('createOrderDto', createOrderDto);
 
-    const createOrder = new OrderEntity(createOrderDto);
-    createOrder.userId = userId;
-    createOrder.totalPrice = totalPrice;
-    console.log('createOrderDto', createOrderDto);
+      const saveOrder = await this.OrderRepository.save(createOrder);
 
-    const saveOrder = await this.OrderRepository.save(createOrder);
-    const orderProducts = await products.map((product) => ({
-      orderId: saveOrder.id,
-      productId: product.id,
-      product_image: product.product_image,
-      quantity: productList.get(product.id),
-    }));
+      const orderProducts = await products.map((product) => ({
+        orderId: saveOrder.id,
+        productId: product.id,
+        product_image: product.product_image,
+        quantity: productList.get(product.id),
+      }));
 
-    await this.ordersProductsRepository.save(orderProducts);
+      await this.ordersProductsRepository.save(orderProducts);
 
-    return { userId, orderProducts };
+      return { userId, orderProducts };
+    }
   }
 
   private getQtyProductById(productsDto: Array<createOrderProductDto>) {
